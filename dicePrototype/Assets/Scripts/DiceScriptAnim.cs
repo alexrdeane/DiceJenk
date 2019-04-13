515b07651a98237768b900249af25cb18c96d3e1
alexrdeane <alexrdeane@hotmail.com>
2019-04-13 19:15:30 +1000

    Add files via upload

---
+++ dicePrototype/Assets/Scripts/DiceScriptAnim.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceScriptAnim : MonoBehaviour
+{
+
+    static Rigidbody rb;
+    public static Vector3 diceVelocity;
+    public Animator[] anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        diceVelocity = rb.velocity;
+
+        if (diceVelocity.x == 0f && diceVelocity.y == 0f && diceVelocity.z == 0f)
+        {
+            anim[0].SetBool("isFalling", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DiceNumberTextScript.diceNumber = 0;
+            float dirX = Random.Range(0, 500);
+            float dirY = Random.Range(0, 500);
+            float dirZ = Random.Range(0, 500);
+            transform.position = new Vector3(0, 2, 0);
+            transform.rotation = Quaternion.identity;
+            rb.AddForce(transform.up * 500);
+            rb.AddTorque(dirX, dirY, dirZ);
+        }
+    }
+}
