bd39454a19afa83ac16f05b11c82c7eef6ae37f9
Alex Deane <alexrdeane@hotmail.com>
2019-06-06 01:27:26 +1000

    initial commit

---
+++ Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    public Camera attachedCamera;
+    public float xSpeed = 120f, ySpeed = 120f;
+    public float minYAngle = 30f, maxYAngle = 80f;
+
+    void Rotate()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        Vector3 euler = transform.eulerAngles;
+
+        euler.x -= mouseY * ySpeed * Time.deltaTime;
+        euler.y += mouseX * ySpeed * Time.deltaTime;
+
+        transform.eulerAngles = euler;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Rotate();
+        }
+    }
+}
